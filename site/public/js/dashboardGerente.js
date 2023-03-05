c8ac13678b444d4428a76f6631ae84a89455e518
didiegovieira <diego.vieira@sptech.school>
2023-03-05 18:42:20 -0300

    Terminada a tela do Gerente, referente ao cadastro de seus funcionarios

---
+++ site/public/js/dashboardGerente.js
@@ -0,0 +1,15 @@
+
+
+function tela() {
+    var elemento = document.querySelector(".body2");
+    
+    elemento.style.display = "flex";
+    
+
+}
+
+function tela2() {
+    var elemento = document.querySelector(".body2");
+    
+    elemento.style.display = "none";
+}
