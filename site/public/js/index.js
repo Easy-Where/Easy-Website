25da41fac3bb7015927229ebcd069d857c0c710b
AugustoCesar15 <augusto.costa@sptech.school>
2023-03-06 10:09:34 -0300

    Finalizada a tela sobre

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,3 +1,15 @@
 function pageHome() {
     window.location = "./index.html"
 }
+
+// Navbar Responsividade
+function mostrarNav() {
+    document.getElementById('nave').style.left = "0"
+    document.getElementById('fe-nave').style.left = "47%"
+}
+
+function feNave() {
+    
+    document.getElementById('nave').style.left = "-80%"
+    document.getElementById('fe-nave').style.left = "-70%"
+}
