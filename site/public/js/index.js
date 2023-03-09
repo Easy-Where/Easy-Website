aa6387b772a9bbfe39932e6115282e1a90985e4c
‘AugustoCesar15’ <augusto.costa@sptech.school>
2023-03-09 14:00:08 -0300

    Mudança de estrutura do site

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -4,12 +4,29 @@ function pageHome() {
 
 // Navbar Responsividade
 function mostrarNav() {
-    document.getElementById('nave').style.left = "0"
-    document.getElementById('fe-nave').style.left = "47%"
+    document.getElementById('nave').style.top = "0"
+    document.getElementById('fe-nave').style.top = "3%"
 }
 
 function feNave() {
     
-    document.getElementById('nave').style.left = "-80%"
-    document.getElementById('fe-nave').style.left = "-70%"
+    document.getElementById('nave').style.top = "-110%"
+    document.getElementById('fe-nave').style.top = "-70%"
 }
+
+var pagina = document.getElementById('section').style
+
+function inicio() {
+    pagina.right = 0
+
+}
+
+function meio() {
+    pagina.right = "100%"
+    feNave()
+}
+function fim() {
+    pagina.right = "200%"
+    feNave()
+}
+
