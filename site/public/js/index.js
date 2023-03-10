5406f6b1a390eaf0b6832c9078ab71f5992a359c
‘AugustoCesar15’ <augusto.costa@sptech.school>
2023-03-10 10:20:07 -0300

    Finalizando ajustes

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -16,17 +16,26 @@ function feNave() {
 
 var pagina = document.getElementById('section').style
 
+var efeito2 = document.getElementById('be').classList
+var efeito3 = document.getElementById('nome').classList
+
 function inicio() {
     pagina.right = 0
+    efeito2.remove('efeito2')
+    efeito3.remove('efeito3')
 
 }
 
 function meio() {
     pagina.right = "100%"
     feNave()
+    efeito2.add('efeito2')
+    efeito3.remove('efeito3')
 }
 function fim() {
     pagina.right = "200%"
     feNave()
+    efeito3.add('efeito3')
+    efeito2.remove('efeito2')
 }
 
