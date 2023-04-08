ab20be823edd8de9fe24aeda1d4731154d3abb5f
PauloCezaretto <paulo.alvares@sptech.school>
2023-04-08 03:29:02 -0300

    Completando vantagens com histórias de usuário, incluindo página de dowload e links de sites

---
+++ site/public/js/easyware.js
@@ -0,0 +1,7 @@
+// Redes Sociais
+let menu = document.querySelector('.menu')
+let toggle = document.querySelector('.toggle')
+let textofooter = document.querySelector('h1#textofooter')
+toggle.onclick = function () {
+    menu.classList.toggle('active')
+}
