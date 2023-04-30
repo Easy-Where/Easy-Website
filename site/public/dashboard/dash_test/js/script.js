fa1f2717987da00f7501019d1cd7852507ca5b3e
PauloCezaretto <paulo.alvares@sptech.school>
2023-04-30 00:59:56 -0300

    Finalizando sidebar dashboard

--- site/public/dashboard/dash_test/js/script.js
+++ site/public/dashboard/dash_test/js/script.js
@@ -1,16 +1,12 @@
-const body = document.querySelector("body"),
-    modeToggle = body.querySelector(".mode-toggle");
+const body = document.querySelector("body")
+const modeToggle = body.querySelector(".mode-toggle");
+const img = document.querySelector("#Logo")
 sidebar = body.querySelector("nav");
 sidebarToggle = body.querySelector(".sidebar-toggle");
 
 let getMode = localStorage.getItem("mode");
 if (getMode && getMode === "dark") {
     body.classList.toggle("dark");
-}
-
-let getStatus = localStorage.getItem("status");
-if (getStatus && getStatus === "close") {
-    sidebar.classList.toggle("close");
 }
 
 modeToggle.addEventListener("click", () => {
@@ -22,11 +18,18 @@ modeToggle.addEventListener("click", () => {
     }
 });
 
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+    sidebar.classList.toggle("close");
+}
+
 sidebarToggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
     if (sidebar.classList.contains("close")) {
         localStorage.setItem("status", "close");
+        img.src = 'assets/easy-ware-logotipo-close.png'
     } else {
         localStorage.setItem("status", "open");
+        img.src = 'assets/easy-ware-logotipo-light.png'
     }
 })
