385b1622b693fa3762c5e9e7185135f10b26a9dd
PauloCezaretto <paulo.alvares@sptech.school>
2023-05-01 12:52:26 -0300

    Inserindo teste com ApexChart

--- site/public/dashboard/dash_test/js/script.js
+++ site/public/dashboard/dash_test/js/script.js
@@ -33,3 +33,25 @@ sidebarToggle.addEventListener("click", () => {
         img.src = 'assets/easy-ware-logotipo-light.png'
     }
 })
+
+// teste de gráfico
+var options = {
+    chart: {
+        type: 'area'
+    },
+    series: [{
+        data: [[1324508400000, 34], [1324594800000, 54], [1326236400000, 43]]
+    }],
+    xaxis: {
+        type: 'numeric'
+    },
+    responsive: [{
+        breakpoint: undefined,
+        options: {},
+    }]
+
+}
+
+var chart = new ApexCharts(document.querySelector("#chart_cpu"), options);
+
+chart.render();
