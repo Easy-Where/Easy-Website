d24e62893582e5a949475e8cdbaf54eaeeed5fbf
PauloCezaretto <paulo.alvares@sptech.school>
2023-05-09 09:40:07 -0300

    Finalizando front da dashboard do técnico

--- site/public/dashboard/dash_test/js/dashboard.js
+++ site/public/dashboard/dash_test/js/dashboard.js
@@ -222,18 +222,51 @@ new ApexCharts(document.querySelector("#kpi_chart_disco"), options4).render();
 //Gráfico CPU
 var options4 = {
     chart: {
-        height: 200,
-        width: "100%",
+        id: "chart2",
         type: "area",
+        height: 200,
+        foreColor: "#f1f1f1",
         animations: {
             initialAnimation: {
                 enabled: false
             }
         }
     },
+    colors: ["#1175d1"],
+    stroke: {
+        width: 3
+    },
+    grid: {
+        borderColor: "#252525",
+        // clipMarkers: false,
+        yaxis: {
+            lines: {
+                show: false
+            }
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    fill: {
+        gradient: {
+            enabled: true,
+            opacityFrom: 1,
+            opacityTo: 0
+        }
+    },
+    markers: {
+        size: 6,
+        colors: ["#000524"],
+        strokeColor: "#1175d1",
+        strokeWidth: 3
+    },
     series: [
+        // {
+        //     data: data
+        // }
         {
-            name: "Series 1",
+            name: "Uso",
             data: [
                 [1486684800000, 34],
                 [1486771200000, 43],
@@ -244,11 +277,250 @@ var options4 = {
             ]
         }
     ],
+    tooltip: {
+        theme: "dark"
+    },
     xaxis: {
-        type: 'datetime'
+        type: "datetime"
+    },
+    yaxis: {
+        min: 0,
+        tickAmount: 5
     }
 };
 
-var chart = new ApexCharts(document.querySelector("#chart_cpu"), options4);
+new ApexCharts(document.querySelector("#chart_cpu"), options4).render();
 
-chart.render();
+// Gráfico RAM
+var options5 = {
+    chart: {
+        id: "chart2",
+        type: "area",
+        height: 200,
+        foreColor: "#f1f1f1",
+        animations: {
+            initialAnimation: {
+                enabled: false
+            }
+        }
+    },
+    colors: ["#1175d1"],
+    stroke: {
+        width: 3
+    },
+    grid: {
+        borderColor: "#252525",
+        // clipMarkers: false,
+        yaxis: {
+            lines: {
+                show: false
+            }
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    fill: {
+        gradient: {
+            enabled: true,
+            opacityFrom: 1,
+            opacityTo: 0
+        }
+    },
+    markers: {
+        size: 6,
+        colors: ["#000524"],
+        strokeColor: "#1175d1",
+        strokeWidth: 3
+    },
+    series: [
+        // {
+        //     data: data
+        // }
+        {
+            name: "Uso",
+            data: [
+                [1486684800000, 34],
+                [1486771200000, 43],
+                [1486857600000, 31],
+                [1486944000000, 43],
+                [1487030400000, 33],
+                [1487116800000, 52]
+            ]
+        }
+    ],
+    tooltip: {
+        theme: "dark"
+    },
+    xaxis: {
+        type: "datetime"
+    },
+    yaxis: {
+        min: 0,
+        tickAmount: 5
+    }
+};
+
+new ApexCharts(document.querySelector("#chart_ram"), options5).render();
+
+// Gráfico Disco
+var options6 = {
+    chart: {
+        id: "chart2",
+        type: "area",
+        height: 200,
+        foreColor: "#f1f1f1",
+        animations: {
+            initialAnimation: {
+                enabled: false
+            }
+        }
+    },
+    colors: ["#1175d1"],
+    stroke: {
+        width: 3
+    },
+    grid: {
+        borderColor: "#252525",
+        // clipMarkers: false,
+        yaxis: {
+            lines: {
+                show: false
+            }
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    fill: {
+        gradient: {
+            enabled: true,
+            opacityFrom: 1,
+            opacityTo: 0
+        }
+    },
+    markers: {
+        size: 6,
+        colors: ["#000524"],
+        strokeColor: "#1175d1",
+        strokeWidth: 3
+    },
+    series: [
+        // {
+        //     data: data
+        // }
+        {
+            name: "Uso",
+            data: [
+                [1486684800000, 34],
+                [1486771200000, 43],
+                [1486857600000, 31],
+                [1486944000000, 43],
+                [1487030400000, 33],
+                [1487116800000, 52]
+            ]
+        }
+    ],
+    tooltip: {
+        theme: "dark"
+    },
+    xaxis: {
+        type: "datetime"
+    },
+    yaxis: {
+        min: 0,
+        tickAmount: 5
+    }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart_disco"), options6).render();
+
+// Gráfico Rede
+var options7 = {
+    series: [{
+        name: 'Download',
+        type: 'area',
+        data: [44, 55, 31, 47, 31, 43, 26, 41, 31, 47, 33],
+        color: "#1175d1",
+    }, {
+        name: 'Upload',
+        type: 'line',
+        data: [55, 69, 45, 61, 43, 54, 37, 52, 44, 61, 43],
+        color: "#a9c7f1"
+    }],
+    chart: {
+        type: 'area',
+        height: 200,
+        foreColor: "#f1f1f1",
+        animations: {
+            initialAnimation: {
+                enabled: false
+            }
+        }
+    },
+    grid: {
+        borderColor: "#252525",
+        yaxis: {
+            lines: {
+                show: false
+            }
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        curve: 'smooth',
+        width: 3
+    },
+    fill: {
+        type: 'solid',
+        opacity: [0.35, 1],
+        gradient: {
+            enabled: true,
+            opacityFrom: 1,
+            opacityTo: 0
+        }
+    },
+    labels: ['Dec 01', 'Dec 02', 'Dec 03', 'Dec 04', 'Dec 05', 'Dec 06', 'Dec 07', 'Dec 08', 'Dec 09 ', 'Dec 10', 'Dec 11'],
+    markers: {
+        size: 6,
+        colors: ["#000524"],
+        strokeColor: "#1175d1",
+        strokeWidth: 3
+    },
+    tooltip: {
+        theme: "dark"
+    },
+    xaxis: {
+        type: "datetime"
+    },
+    yaxis: [
+        {
+            min: 0,
+            tickAmount: 5
+        },
+    ]
+};
+
+new ApexCharts(document.querySelector("#chart_rede"), options7).render();
+
+var data = generateDayWiseTimeSeries(new Date("22 Apr 2017").getTime(), 115, {
+    min: 30,
+    max: 90
+});
+
+function generateDayWiseTimeSeries(baseval, count, yrange) {
+    var i = 0;
+    var series = [];
+    while (i < count) {
+        var x = baseval;
+        var y =
+            Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+        series.push([x, y]);
+        baseval += 86400000;
+        i++;
+    }
+    return series;
+}
