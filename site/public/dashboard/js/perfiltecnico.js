832e27cbe90a8af764f8495857e23db32a47c55f
PauloCezaretto <paulo.alvares@sptech.school>
2023-05-16 14:00:53 -0300

    Adicionando função ver e desver senha e corrigindo cores nas telas de perfil

--- site/public/dashboard/js/perfiltecnico.js
+++ site/public/dashboard/js/perfiltecnico.js
@@ -1,48 +1,34 @@
 // Ver senha 
 const input = document.querySelector("#senha");
-const img2 = document.querySelector("#trocarFigura");
-img2.addEventListener('click', togglePass);
+const versenha_gestor = document.querySelector("#trocarFigura");
+versenha_gestor.addEventListener('click', togglePass);
 
 function togglePass() {
     if (input.type == "password") {
         input.type = "text";
-        img2.src = 'assets/perfilescondersenha.png'
+        versenha_gestor.classList.replace("uil-eye", "uil-eye-slash")
     } else {
         input.type = "password";
-        img2.src = 'assets/perfilversenha.png'
+        versenha_gestor.classList.replace("uil-eye-slash", "uil-eye")
     }
 }
 
 // Ver confirmação de senha
 const input4 = document.querySelector("#confirmasenha");
-const img4 = document.querySelector("#confirmatrocarFigura");
-img4.addEventListener('click', togglePass4);
+const versenha_gestor2 = document.querySelector("#confirmatrocarFigura");
+versenha_gestor2.addEventListener('click', togglePass4);
 
 function togglePass4() {
     if (input4.type == "password") {
         input4.type = "text";
-        img4.src = 'assets/perfilescondersenha.png'
+        versenha_gestor2.classList.replace("uil-eye", "uil-eye-slash")
     } else {
         input4.type = "password";
-        img4.src = 'assets/perfilversenha.png'
+        versenha_gestor2.classList.replace("uil-eye-slash", "uil-eye")
     }
 }
 
-// Ver senha PID
-const input3 = document.querySelector("#pid");
-const img3 = document.querySelector("#trocarFigurapid");
-img3.addEventListener('click', togglePass3);
-
-function togglePass3() {
-    if (input3.type == "password") {
-        input3.type = "text";
-        img3.src = 'assets/perfilescondersenha.png'
-    } else {
-        input3.type = "password";
-        img3.src = 'assets/perfilversenha.png'
-    }
-}
-
+// Trocar Imagem
 const image = document.getElementById("imagem_perfil")
 const image2 = document.getElementById("imagem_perfil_sidebar")
 const input_trocavel = document.getElementById("input_troca_imagem")
@@ -51,3 +37,12 @@ input_trocavel.addEventListener("change", () => {
     image.src = URL.createObjectURL(input_trocavel.files[0]);
     image2.src = URL.createObjectURL(input_trocavel.files[0]);
 });
+
+// Resetar inputs
+function resetarInputs() {
+    nome_input.value = ''
+    sobrenome_input.value = ''
+    email_input.value = ''
+    senha.value = ''
+    confirmasenha.value = ''
+}
