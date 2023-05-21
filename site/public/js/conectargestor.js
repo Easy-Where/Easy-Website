d8ee3852640bf93503db3adfdb0c2ddb0cf5b950
PauloCezaretto <paulo.alvares@sptech.school>
2023-05-21 03:48:17 -0300

    Modificando funções de cadastro e etapa de cadastro

--- site/public/js/conectargestor.js
+++ site/public/js/conectargestor.js
@@ -11,7 +11,7 @@ signInButton.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
 });
 
-// Div de validação
+// Modal de erro
 function modalErro(frase1, frase2) {
   var divValidacao = document.querySelector(".validacao");
   var textModal = document.querySelector(".titulo_validacao");
@@ -24,16 +24,15 @@ function modalErro(frase1, frase2) {
   }, 3000)
 }
 
+// Modal de sucesso
 function modalSucesso(frase1, frase2) {
   var divValidacao = document.querySelector(".validacao");
   var textModal = document.querySelector(".titulo_validacao");
   var textValidacao = document.querySelector(".texto_validacao");
 
   textModal.innerHTML = frase1;
-  textModal.style.background = "green";
-  textModal.style.color = "white";
-  textModal.style.fontSize = "1.1em";
   textValidacao.innerHTML = frase2;
+  textModal.style.background = "#1175d1";
 
   divValidacao.classList.add("active");
   setTimeout(() => {
@@ -52,101 +51,59 @@ function verSenha(input, icone) {
   }
 }
 
-// Progresso da tela de cadastro
-const nextBtn = document.querySelectorAll('#next-btn');
-const nextBtn2 = document.querySelectorAll('#next-btn2');
+// Avançar etapa de cadastro
+const slideAtual = document.querySelector(".page");
 
-const prevBtn = document.querySelectorAll('#prev-btn');
-const prevBtn2 = document.querySelectorAll('#prev-btn2');
-const pages = document.querySelectorAll('.page');
-const progress = document.querySelectorAll('.progresso-bar')
+// Cadastro de empresa
+function avancaPraDois() {
+  const nome = nome_input.value;
+  const telefone = telefone_input.value;
+  const email = emailCad_input.value;
+  let validacaoEmail = /^(\w{2,})([._]?\w+)*@(\w{3,})([._]\w{2,})?([.-])[\w]{2,}$/;
 
-let currentPage = 0;
-
-function showCurrentPage(index) {
-  pages.forEach((page, i) => {
-    if (i == index) {
-      page.classList.add('active');
-    } else {
-      page.classList.remove('active');
-    }
-  });
-
-  progress.forEach((progressBar, i) => {
-    if (i == index) {
-      progressBar.classList.add('current');
-    } else {
-      progressBar.classList.remove('current');
-    }
-  })
+  if (nome == "") {
+    modalErro("Campo vazio", "&quotNome&quot está vazio")
+  } else if (telefone == "") {
+    modalErro("Campo vazio", "&quotTelefone&quot está vazio")
+  } else if(telefone.length < 15){
+    modalErro("Dado incorreto", "&quotTelefone&quot está incompleto")
+  } else if (email == "") {
+    modalErro("Campo vazio", "&quotEmail&quot está vazio")
+  } else if (!validacaoEmail.test(email)) {
+    modalErro("Dado incorreto", "E-mail inválido! Certifique-se que<br>seu e-mail segue essa estrutura: nome@example.com")
+  } else {
+    slideAtual.style.marginLeft = "-300px";
+  }
 };
 
-function addProgress(index) {
-  progress[index].classList.add('active');
-}
+// Finalizar cadastro
+function avancaPraTres() {
+  const empresa = empresa_input.value;
+  const cnpj = cnpj_input.value;
+  const dono = dono_input.value;
 
-function subProgress(index) {
-  progress[index + 1].classList.remove('active');
-}
+  if (empresa == "") {
+    modalErro("Campo vazio", "&quotEmpresa&quot está vazio")
+  } else if (cnpj == "") {
+    modalErro("Campo vazio", "&quotCPNJ&quot está vazio")
+  } else if (cnpj.length < 18) {
+    modalErro("Dado incorreto", "CNPJ está incompleto")
+  } else if (dono == "") {
+    modalErro("Campo vazio", "&quotDono&quot está vazio")
+  } else {
+    slideAtual.style.marginLeft = "-600px";
+  }
+};
 
-function handleNext() {
-  currentPage++;
-  showCurrentPage(currentPage);
-  addProgress(currentPage);
-}
+// Cadastro de usuário
+function voltaPraUm() {
+  slideAtual.style.marginLeft = "0px";
+};
 
-function handlePrev() {
-  currentPage--;
-  showCurrentPage(currentPage);
-  subProgress(currentPage);
-}
-
-nextBtn.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const nome = nome_input.value;
-    const email = emailCad_input.value;
-    var validacaoEmail = /^(\w{2,})([._]?\w+)*@(\w{3,})([._]\w{2,})?([.-])[\w]{2,}$/;
-
-    if (nome == "") {
-      modalErro("ERRO", "O campo Nome está vazio")
-    } else if (email == "") {
-      modalErro("ERRO", "O campo Email está vazio")
-    } else if (!validacaoEmail.test(email)) {
-      modalErro("ERRO", "E-mail inválido! Certifique-se que<BR> seu e-mail segue essa estrutura: nome@example.com")
-    } else {
-      handleNext();
-    }
-  });
-});
-
-nextBtn2.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const empresa = empresa_input.value;
-    const cnpj = cnpj_input.value;
-    const dono = dono_input.value;
-
-    if (empresa == "") {
-      modalErro("ERRO", "O campo Empresa está vazio")
-    } else if (cnpj == "") {
-      modalErro("ERRO", "O campo CPNJ está vazio")
-    } else if (dono == "") {
-      modalErro("ERRO", "O campo Dono está vazio")
-    } else if (cnpj.length < 18) {
-      modalErro("ERRO", "O campo CNPJ está menor do que o ideal")
-    } else {
-      handleNext();
-    }
-  });
-});
-
-
-prevBtn.forEach(btn => {
-  btn.addEventListener('click', handlePrev);
-});
-
-prevBtn2.forEach(btn => {
-  btn.addEventListener('click', handlePrev);
-});
+// Cadastro de empresa
+function voltaPraDois() {
+  slideAtual.style.marginLeft = "-300px";
+};
 
 // Padronizar CNPJ
 function maskCNPJ(event) {
@@ -174,17 +131,17 @@ function maskCNPJ(event) {
 // Padronizar Telefone
 function maskPhone(event) {
   let phone = telefone_input.value.replace(/\D+/g, "").trim()
-  
+
   if (phone.length > 11) {
-      return false
+    return false
   }
 
   if (phone.length > 10) {
-      phone = phone.replace(/^(\d{2})(\d{5})(\d{4})/, "($1) $2-$3")
+    phone = phone.replace(/^(\d{2})(\d{5})(\d{4})/, "($1) $2-$3")
   } else if (phone.length > 6) {
-      phone = phone.replace(/^(\d{2})(\d{4})/, "($1) $2-")
+    phone = phone.replace(/^(\d{2})(\d{4})/, "($1) $2-")
   } else if (phone.length > 1) {
-      phone = phone.replace(/^(\d{2})/, "($1) ")
+    phone = phone.replace(/^(\d{2})/, "($1) ")
   }
 
   telefone_input.value = phone
@@ -199,7 +156,7 @@ function logar() {
     modalErro("ERRO", "O campo E-mail está vazio")
   } else if (senhaVar == "") {
     modalErro("ERRO", "O campo Senha está vazio")
-  } 
+  }
 
   console.log("FORM LOGIN: ", emailVar);
   console.log("FORM SENHA: ", senhaVar);
@@ -256,13 +213,13 @@ function cadastrar() {
   var confirmacao = confirma_input.value;
 
   if (senhaVar == "") {
-    modalErro("ERRO", "O campo Senha está vazio")
+    modalErro("Campo vazio", "&quotSenha&quot está vazio")
   } else if (confirmacao == "") {
-    modalErro("ERRO", "O campo Confirmação de Senha está vazio")
+    modalErro("Campo vazio", "&quotConfirmação de Senha&quot está vazio")
   } else if (senhaVar.length <= 8) {
-    modalErro("ERRO", "A senha deve ter mais de 8 caracteres")
+    modalErro("Aumente a segurança", "A senha deve ter mais de 8 caracteres")
   } else if (confirmacao != senhaVar) {
-    modalErro("ERRO", "Senhas diferentes")
+    modalErro("Dado incorreto", "Senhas diferentes")
   }
 
   fetch("/usuarios/validacao", {
@@ -278,94 +235,72 @@ function cadastrar() {
 
   }).then(function (resposta) {
     if (resposta.ok) {
-      modalSucesso("CADASTRO REALIZADO COM SUCESSO!", "Agora vamos fazer Login :)")
+      modalSucesso("Cadastro realizado!", "Vamos fazer login?")
       resposta.json().then(function (resposta) {
         console.log(resposta)
         if (resposta.length < 1) {
           console.log("Cadastrando empresa...");
-          console.log(resposta)
-          cadastrarEmpresa()
+          console.log(resposta);
+          fetch("/usuarios/cadastrarEmpresa", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              empresaServer: empresaVar,
+              cnpjServer: cnpjVar,
+              donoServer: donoVar
+            })
+          }).then(function (resposta) {
+            console.log(resposta)
+            if (resposta.ok) {
+              console.log("Empresa cadastrada com sucesso");
+              cadastrar();
+            } else {
+              console.log("Deu erro ao cadastrar Empresa");
+            }
+          }).catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+          });
         } else {
           console.log("Cadastrando gestor...");
           console.log(resposta[0].id_empresa);
-          cadastrarGestor(resposta[0].id_empresa);
+          let nomeVar = nome_input.value;
+          let emailVar = emailCad_input.value;
+          let empresaVar = resposta[0].id_empresa;
+          let senhaVar = senha_input.value;
+
+          fetch("/usuarios/cadastrarGestor", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              nomeServer: nomeVar,
+              emailServer: emailVar,
+              senhaServer: senhaVar,
+              empresaServer: empresaVar,
+            })
+          }).then(function (resposta) {
+            console.log("resposta: ", resposta);
+
+            if (resposta.ok) {
+              console.log("Gestor cadastrado com sucesso");
+              setTimeout(() => {
+                window.location = "conectargestor.html";
+              }, "2000");
+            } else {
+              throw ("Houve um erro ao tentar realizar o cadastro!");
+            }
+          }).catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+          });
         }
       });
     } else {
       console.log("Erro ao validar dados");
     }
-
-  }).catch(function (resposta) {
-    console.log(`#ERRO: ${resposta}`);
-  });
-
-}
-
-
-function cadastrarEmpresa() {
-  var empresaVar = empresa_input.value;
-  var cnpjVar = cnpj_input.value.replace(/\D+/g, "").trim();
-  var donoVar = dono_input.value;
-
-  console.log("Estou no CadastrarEmpresa");
-
-  fetch("/usuarios/cadastrarEmpresa", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      empresaServer: empresaVar,
-      cnpjServer: cnpjVar,
-      donoServer: donoVar
-    })
-  }).then(function (resposta) {
-    console.log(resposta)
-    if (resposta.ok) {
-      console.log("Empresa cadastrada com sucesso");
-      cadastrar();
-    } else {
-      console.log("Deu erro ao cadastrar Empresa");
-    }
-
   }).catch(function (resposta) {
     console.log(`#ERRO: ${resposta}`);
   });
 }
-
-function cadastrarGestor(empresaId) {
-  var nomeVar = nome_input.value;
-  var emailVar = emailCad_input.value;
-  var empresaVar = empresaId;
-  var senhaVar = senha_input.value;
-
-  fetch("/usuarios/cadastrarGestor", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      nomeServer: nomeVar,
-      emailServer: emailVar,
-      senhaServer: senhaVar,
-      empresaServer: empresaVar,
-
-    })
-  }).then(function (resposta) {
-    console.log("resposta: ", resposta);
-
-    if (resposta.ok) {
-      console.log("Gestor cadastrado com sucesso");
-      setTimeout(() => {
-        window.location = "conectargestor.html";
-      }, "2000");
-    } else {
-      throw ("Houve um erro ao tentar realizar o cadastro!");
-    }
-  }).catch(function (resposta) {
-    console.log(`#ERRO: ${resposta}`);
-  });
-
-  return false;
-}
-
