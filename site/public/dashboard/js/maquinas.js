488b25ef692c8ab0b9c3547eafd08911e5f3a134
Augusto Costa <augusto.costa@boxdelivery.com.br>
2023-05-30 17:07:25 -0300

    WIP

---
+++ site/public/dashboard/js/maquinas.js
@@ -0,0 +1,28 @@
+let layoutModalMaquina = document.querySelector(".modalCadastroMaquina")
+let layoutModalFuncioanrio = document.querySelector(".modalCadastroFuncionario")
+let modalMaquina = document.getElementById('modal-maquina')
+
+
+function cadastrarMaquina(){
+    // layoutModalMaquina.style.top = "0"
+    layoutModalMaquina.classList.add('fundo')
+}
+
+function closeModalMaquina(){
+    // layoutModalMaquina.style.top = "-100vh"
+    // modalMaquina.style.left = "-300%"
+    layoutModalMaquina.classList.remove('fundo')
+
+}
+
+function cadastrarFuncionario(){
+    // modalMaquina.style.left = "30%"
+    layoutModalFuncioanrio.classList.add('fundo')
+}
+
+function closeModalFuncionario(){
+    // layoutModalMaquina.style.top = "-100vh"
+    // modalMaquina.style.left = "-300%"
+    layoutModalFuncioanrio.classList.remove('fundo')
+
+}
